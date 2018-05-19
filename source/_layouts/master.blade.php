b0316bbe63dbf3de68c312b53f836a9a627fea2b
Matt Stauffer <matt@tighten.co>
2018-05-19 18:17:50 -0400

    Better mobile

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -10,10 +10,12 @@
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
     </head>
     <body>
-        <div class="container mx-auto mt-8 pt-8 px-4">
-            @yield('body')
+        <div class="container mx-auto mt-8 pt-8">
+            <div class="px-4">
+                @yield('body')
+            </div>
 
-            <div class="mt-8 pt-8 border-t border-grey-light text-center">
+            <div class="px-4 mt-8 pt-8 border-t border-grey-light text-center">
                 <a href="http://jigsaw.tighten.co/">Jigsaw</a> is a static site generator brought to you by <a href="https://tighten.co/">Tighten</a>. This site is <a href="https://github.com/tightenco/builtwithjigsaw">open source on GitHub</a>.
             </div>
         </div>
