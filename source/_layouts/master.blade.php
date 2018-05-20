44f65924939bb4a661cd7aff06ba79231c3f99ae
Matt Stauffer <mattstauffer@users.noreply.github.com>
2018-05-20 09:13:35 -0400

    Add  x padding to intro paragraph for mobile

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -24,7 +24,7 @@
                 <h1>Built <span class="lowercase italic text-grey-darkest">with</span> Jigsaw</h1>
             </div>
 
-            <div class="mb-8 md:w-1/2 mx-auto leading-normal">
+            <div class="mb-8 md:w-1/2 mx-auto leading-normal px-4">
                 Want to add an article you wrote or found about Jigsaw? Or add your site or project to our list? Add a <a href="https://github.com/tightenco/builtwithjigsaw/compare">Pull Request</a> if you feel comfortable, but if not, just add an <a href="https://github.com/tightenco/builtwithjigsaw/issues/new">Issue</a> and we'll add the code for you.
             </div>
 
