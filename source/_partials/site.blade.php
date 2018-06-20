80eadc1bc83f36121f87f457a41600f38489dad2
Matt Stauffer <matt@tighten.co>
2018-06-20 14:48:29 -0400

    Drop thingy classes

--- source/_partials/site.blade.php
+++ source/_partials/site.blade.php
@@ -15,7 +15,7 @@ $image = file_exists(getcwd() . '/source' . $imagePath) ? $imagePath : '/assets/
 <div class="flex m-2 shadow hover:shadow-md" style="width: 380px;">
     <div class="flex-1">
         <a href="{{ $site->url }}" class="block"><img src="{{ $image }}" alt="Web site screenshot" class="border"></a>
-        <div class="middle-thingy text-sm py-4 px-6 bg-white">
+        <div class="text-sm py-4 px-6 bg-white">
             <a href="{{ $site->url }}" class="no-underline text-grey-darkest mb-1 block">{{ $site->title }}</a>
             <div class="mb-4 text-grey-dark">by {{ implode($site->authors, ', ') }}</div>
             <div class="mb-2">
@@ -24,7 +24,7 @@ $image = file_exists(getcwd() . '/source' . $imagePath) ? $imagePath : '/assets/
                 @endforeach
             </div>
         </div>
-        <div class="bottom-thingy bg-white flex text-sm border-t">
+        <div class="bg-white flex text-sm border-t">
             <a href="{{ $site->url }}" class="p-4 flex-1 text-center p-4 no-underline text-blue hover:bg-blue hover:text-white">{{--[icon]--}} Visit website</a>
             @if ($site->repo)
             <a href="{{ $site->repo }}" class="p-4 flex-1 text-center p-4 no-underline text-blue hover:bg-blue hover:text-white">{{--[icon]--}} Visit repo</a>
