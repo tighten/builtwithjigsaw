c6fd78fa362964a053710d39abd9311eae061460
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2022-09-30 18:43:33 -0400

    Fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,7 @@ mix.disableSuccessNotifications().setPublicPath('source/assets/build');
 
 mix.jigsaw()
     .browserSync({ server: 'build_local', files: ['build_local/**'] })
+    .vue({ version: 2 })
     .js('source/_assets/js/main.js', 'js')
     .css('source/_assets/css/main.css', 'css', [require('tailwindcss')])
     .version();
